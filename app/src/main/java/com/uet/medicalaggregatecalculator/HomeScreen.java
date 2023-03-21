973b1eaaf855dc59e9065af6ec837d4793670f2d
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-03-21 15:09:39 +0500

    added null check in fields

--- app/src/main/java/com/uet/medicalaggregatecalculator/HomeScreen.java
+++ app/src/main/java/com/uet/medicalaggregatecalculator/HomeScreen.java
@@ -13,28 +13,34 @@ public class HomeScreen extends AppCompatActivity {
     public void calculate(View view) {
         EditText m1 = findViewById(R.id.matricMarks);
         String marks1 = m1.getText().toString();
-        int matricMarks = Integer.parseInt(marks1);
         EditText m2 = findViewById(R.id.fscMarks);
         String marks2 = m2.getText().toString();
-        int fscMarks = Integer.parseInt(marks2);
         EditText m3 = findViewById(R.id.entryTestMarks);
         String marks3 = m3.getText().toString();
-        int entryMarks = Integer.parseInt(marks3);
 
-        double p1 = ((matricMarks/1100d)*100)*0.1d;
+        if (marks1.isEmpty() || marks2.isEmpty() || marks3.isEmpty()) {
+            Toast.makeText(this, "Enter all fields first.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            int matricMarks = Integer.parseInt(marks1);
+            int fscMarks = Integer.parseInt(marks2);
+            int entryMarks = Integer.parseInt(marks3);
 
-        double p2 = ((fscMarks/1100d)*100)*0.4d;
+            double p1 = ((matricMarks/1100d)*100)*0.1d;
 
-        double p3 = ((entryMarks/200d)*100)*0.5d;
+            double p2 = ((fscMarks/1100d)*100)*0.4d;
 
-        double aggregate = p1 + p2 + p3;
-        String output = String.valueOf(aggregate);
+            double p3 = ((entryMarks/200d)*100)*0.5d;
 
-        TextView res = (TextView) findViewById(R.id.id_result);
+            double aggregate = p1 + p2 + p3;
+            String output = String.valueOf(aggregate);
 
-        res.setText(String.valueOf(aggregate));
+            TextView res = (TextView) findViewById(R.id.id_result);
 
-//        Toast.makeText(this, output, Toast.LENGTH_SHORT).show();
+            res.setText(output);
+        }
+
+
     }
 
     @Override
